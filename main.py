54f50ae2218d579062aa57c887d0fd6342271819
Farid Nurrahman <fnur0006@student.monash.edu>
2026-07-18 15:02:33 +1000

    basic commands + server check

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from enum import member
 import os
 
 import discord
@@ -9,6 +8,8 @@ load_dotenv()
 
 bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
 
+ALLOWED_SERVER = 1527856371884884048
+STAFF_ROLE = 1527871437489045514
 
 @bot.event
 async def on_ready():
@@ -17,8 +18,22 @@ async def on_ready():
 
 
 @bot.tree.command(name="ping", description="pong!")
-async def ping(interaction: discord.Interaction):
-    await interaction.response.send_message("pong!", ephemeral=True)
+async def ping(ctx: discord.Interaction):
+    await ctx.response.send_message("pong!", ephemeral=True)
+
+@bot.tree.command(name="register")
+async def register(ctx: discord.Interaction, nick: str, irc_password: str):
+    if STAFF_ROLE not in [role.id for role in ctx.user.roles] or ctx.guild_id != ALLOWED_SERVER:
+        return # nuh uh
+    
+    await ctx.response.send_message("register cmd", ephemeral=True)
+
+@bot.tree.command(name="connect", description="connect to irc")
+async def connect(ctx: discord.Interaction):
+    if STAFF_ROLE not in [role.id for role in ctx.user.roles] or ctx.guild_id != ALLOWED_SERVER:
+        return
+
+    await ctx.response.send_message("cnnect cmd", ephemeral=True)
 
 
 bot.run(os.getenv("BOT_TOKEN"))
